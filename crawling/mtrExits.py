216a5088a3bccb09318d88e6d7ca283befe934a9
wongcht <16640500+wongcht@users.noreply.github.com>
2026-04-27 01:23:26 +0100

    get lat lng from iGeoCom data instead of API query

--- crawling/mtrExits.py
+++ crawling/mtrExits.py
@@ -4,38 +4,13 @@ import csv
 import json
 import logging
 import re
-import string
 
 import httpx
 from crawl_utils import emitRequest
-from pyproj import Transformer
 from utils import DATA_DIR, query_igeocom_geojson
 
 res = []
 mtrStops = {}
-epsgTransformer = Transformer.from_crs("epsg:2326", "epsg:4326")
-
-
-def checkResult(results, q, stop, exit, barrierFree):
-    for result in results:
-        if result["nameZH"] == q:
-            lat, lng = epsgTransformer.transform(result["y"], result["x"])
-            res.append(
-                {
-                    "name_en": stop["name_en"],
-                    "name_zh": stop["name_tc"],
-                    "name": {
-                        "en": stop["name_en"],
-                        "zh": stop["name_tc"],
-                    },
-                    "exit": exit,
-                    "lat": lat,
-                    "lng": lng,
-                    "barrierFree": barrierFree,
-                }
-            )
-            return True
-    return False
 
 
 async def main():
@@ -119,20 +94,22 @@ async def main():
                 exit_code_no_digit in barrier_free_exits
             )
 
-            pass
+            lng, lat = feature["geometry"]["coordinates"]
 
-        q = "港鐵" + stop["name_tc"] + "站進出口"
-        r = await emitRequest(
-            "https://geodata.gov.hk/gs/api/v1.0.0/locationSearch?q=" + q, a_client
-        )
-        for char in string.ascii_uppercase:
-            q = "港鐵" + stop["name_tc"] + "站-" + str(char) + "進出口"
-            checkResult(r.json(), q, stop, char, str(char) in stop)
-            for i in range(1, 10):
-                q = "港鐵" + stop["name_tc"] + "站-" + char + str(i) + "進出口"
-                checkResult(
-                    r.json(), q, stop, char + str(i), (char + str(char)) in stop
-                )
+            res.append(
+                {
+                    "name_en": stop["name_en"],
+                    "name_zh": stop["name_tc"],
+                    "name": {
+                        "en": stop["name_en"],
+                        "zh": stop["name_tc"],
+                    },
+                    "exit": exit_code,
+                    "lat": lat,
+                    "lng": lng,
+                    "barrierFree": is_barrier_free,
+                }
+            )
 
     with open(DATA_DIR / "exits.mtr.json", "w", encoding="UTF-8") as f:
         f.write(
