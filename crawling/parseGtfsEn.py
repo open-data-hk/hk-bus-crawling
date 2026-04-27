0df7bf5dd63cc0fc8890aa44ff812136b17dd9ea
wongcht <16640500+wongcht@users.noreply.github.com>
2026-04-27 09:12:34 +0100

    remove parseGtfsEn.py - can pass "en" as arg in parseGtfs.py

--- crawling/parseGtfsEn.py
+++
@@ -1,9 +0,0 @@
-import asyncio
-import logging
-
-from parseGtfs import parseGtfs
-
-if __name__ == "__main__":
-    logging.basicConfig(level=logging.INFO)
-    logging.getLogger("httpx").setLevel(logging.WARNING)
-    asyncio.run(parseGtfs("en"))
