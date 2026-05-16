448277e7d060211d352cc57fc6569b34a6afa451
wongcht <16640500+wongcht@users.noreply.github.com>
2026-05-16 12:24:22 +0100

    revise route_key in kmb

--- crawling/operator/kmb.py
+++ crawling/operator/kmb.py
@@ -4,4 +4,6 @@ from .base_operator import BaseOperator
 class KMB(BaseOperator):
     @classmethod
     def route_key(cls, route):
-        return super().route_key(route)
+        route_no = route.get("route")
+        service_type = route.get("service_type")
+        return f"kmb|{route_no}|{route['bound']}|{service_type}"
